b61b1edc6ee7b9a7a92fffc0f8192eb4b8f84c98
Leonardo Frangelli <leonardo.frangelli@raisin.com>
2019-04-28 13:12:49 +0200

    feat(All): Fix babel loader and bootstraping React App

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlubin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "src/index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "build.js"
